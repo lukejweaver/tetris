ab1616409307ab5676888160c610d187e262311e
Luke Weaver <lukejweaver01@gmail.com>
2021-03-12 22:12:50 -0500

    Create BlockBlueprints interface

---
+++ src/tetris/BlockBlueprints.java
@@ -0,0 +1,39 @@
+package tetris;
+
+public interface BlockBlueprints {
+
+	String TBLOCK = "----/"
+			  	  + "-+--/"
+			  	  + "+++-/"
+			  	  + "----";
+
+	String LBLOCK = "----/"
+		  	  	  + "-+--/"
+		  	  	  + "-+--/"
+		  	  	  + "-++-";
+	
+	String IBLOCK = "-+--/"
+		  	      + "-+--/"
+		  	      + "-+--/"
+		  	      + "-+--";
+	
+	String JBLOCK = "----/"
+		  	      + "-+--/"
+		  	      + "-+--/"
+		  	      + "++--";
+	
+	String ZBLOCK = "----/"
+		  	      + "-+--/"
+		  	      + "++--/"
+		  	      + "+---";
+	
+	String SBLOCK = "----/"
+		  	      + "-+--/"
+		  	      + "-++-/"
+		  	      + "--+-";
+	
+	String OBLOCK = "----/"
+		  	      + "----/"
+		  	      + "-++-/"
+		  	      + "-++-";
+}
