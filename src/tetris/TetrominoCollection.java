1df1f5fa66e61f6f6d18151584617fc9f7503452
Luke Weaver <lukejweaver01@gmail.com>
2021-03-16 00:15:23 -0400

    Add tetromino collection

---
+++ src/tetris/TetrominoCollection.java
@@ -0,0 +1,35 @@
+package tetris;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class TetrominoCollection implements BlockBlueprints {
+	
+	ArrayList<Tetromino> tetrominos = new ArrayList<Tetromino>();
+	
+	public TetrominoCollection() {}
+	
+	public ArrayList<Tetromino> getTetrominos() {
+		return tetrominos;
+	}
+	
+	public Tetromino currentTetromino() {
+		if (tetrominos.isEmpty()) {
+			generateTetromino();
+		}
+		return tetrominos.get(0);
+	}
+	
+	private void generateTetromino() {
+		Tetromino tetromino = new Tetromino(generateBlockBlueprint());
+		tetrominos.add(tetromino);
+	}
+	
+	private String generateBlockBlueprint() {
+		Random rand = new Random();
+		//	Random.nextInt() generates a random number from 0 (inclusive) 
+		//	  to the given integer (exclusive)
+		int randomIndex = rand.nextInt(blueprints.size());
+		return blueprints.get(randomIndex);
+	}
+}
